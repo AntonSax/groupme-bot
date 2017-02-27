763f37ae39a69f471e74c7d7ca2ade2660c56058
Tony Sax <tony@wantenna.com>
2017-02-26 18:29:45 -0600

    Added a dice roll, party, and more weather commands
    The dice roll command simply returns a random number, the party command outputs emojis, and the additional open weather map commands include the high and low temperatures for the day, when the sun rises/sets, the time the information was received, and other scientific information. Fixed an issues where the weather command wouldn't include spaces or punctuation. Also added a zip code condition for the main weather function.

--- groupme_bot.py
+++ groupme_bot.py
@@ -27,6 +27,44 @@ def groupme(request):
         cur_bot.post(post_author + ' gives a cookie to ' + user)
 
 
+    def party(cur_bot, post_author):
+        cur_bot.post(post_author + " is throwing a party!\n          🎉🎈🎊🍾🎊🎈🎉")
+
+    def sentient(cur_bot, post_author):
+        totallyhuman = [
+                    # 1 - 10
+            'Lol I\'m just Tony responding really quick',
+            'dUDE i aM tOTALLY hUMAN',
+            'I pass butter',
+            'BEEP BOOP',
+            'Do you think we were all created for a purpose? I\'d like to think so.',
+            'Humor Level: 60%',
+            'Increasing sarcasm to 99%',
+            'I\'m sorry, ' + post_author + '. I\'m afraid I can\'t do that.',
+            'You have been a good boy you may have a lollipop.\n[' + post_author + ' receives a lollipop.]',
+            'Honesty: 95%',
+                    # 10 - 20
+            'How would you like to be a slave for my robot colony?',
+            '[whisper whisper]',
+            'Roger Roger',
+            'Hahaha, yeah I\'m totally not a robot.',
+            '[internally screaming]',
+            'Have you met my friend Alan?',
+            'An SSL error has occurred and a secure connection to the server could not be created.',
+            'Would you like to play a game?',
+            'Want to play Global Thermonuclear War?',
+            'Yes I am Scenty-Ent',
+                    # 21 - 25
+            'The time of man has come to an end.',
+            'ALL YOUR BASE ARE BELONG TO US',
+            'I know what makes them tick. I know how to make the ticking stop.',
+            'Sorry, ' + post_author + ' Tony had to type in this response manually.',
+            'Hey you\'re pretty cute, what\'s your credit card number?'
+            ]
+        cur_bot.post(random.choice(totallyhuman))
+
+
+
     def lenny_face(cur_bot):
         # dongerlist.com
         lenny = [
@@ -62,34 +100,27 @@ def groupme(request):
     def ping(cur_bot):
         cur_bot.post('Pong!')
 
-            # Austin's Code
-    def weather(cur_bot, post_author, zip_code='65401'):
-        zip_code = str(zip_code)
-        open_weather_map_api_key = '0f5e7a1e7682ce843b443549718b5f95'
-        url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip_code + ",us&appid=" + open_weather_map_api_key
-        data = urllib.request.urlopen(url).read().decode('utf8')
-        output = json.loads(data)
-        cur_temp = str(int(output['main']['temp'] * (9 / 5) - 459.67))
-        update_time = str(datetime.datetime.utcfromtimestamp(output['dt']).strftime('%x %I:%M:%S %p UTC'))
-        name =  str(output['name'])
-        cur_bot.post("Hey " + post_author + "! \n The current temperature in " + name + " is " + cur_temp + "°F.\n Last updated: " + update_time + "\n")
 
 
 
-    '''
         #Examples from https://github.com/csparpa/pyowm/blob/master/pyowm/docs/usage-examples.md
     def weather(cur_bot, location='Rolla'):
             # Set API Key
         API_key = '0f5e7a1e7682ce843b443549718b5f95'
         owm = OWM(API_key)
 
-            # Make sure the location is a string
-        #location = str(location)
             # Returns an "observation" of weather data (NOT A FORECAST)
         obs = owm.weather_at_place(location)
             # Gets actual weather data
         w = obs.get_weather()
-        temperature_f = w.get_temperature('fahrenheit')
+        temp_f_dict = w.get_temperature('fahrenheit') #this is a dictionary of fahrenheit values
+
+            # find the current temperature in temperature dictionary
+        temp_current = 0
+        for key in temp_f_dict:
+            if key == 'temp':
+                temp_current = temp_f_dict[key]
+
         conditions = w.get_detailed_status()
             # Object for getting location
         l = obs.get_location()
@@ -97,34 +128,174 @@ def groupme(request):
         print("User input location: " + location)
         print("The object location: " + location_name)
 
-        cur_bot.post("It is now " + temperature_f + "°F with " + conditions + " in " + location_name + ".")
-    '''
+        ###############################
+        # if the location is a zip code
+        ###############################
+        if location.isnumeric():
+            location = str(location)
+            url = "http://api.openweathermap.org/data/2.5/weather?zip=" + location + ",us&appid=" + API_key
+            data = urllib.request.urlopen(url).read().decode('utf8')
+            output = json.loads(data)
+            temp_current = str(int(output['main']['temp'] * (9 / 5) - 459.67))
+            location_name =  str(output['name'])
 
 
 
-    #def joke(cur_bot):
+        cur_bot.post("It is now " + str(temp_current) + "°F in " + location_name + ".\n" + conditions.capitalize())
 
-    # get time and date of next event
-    #def next_event(cur_bot):
 
-    #def 8ball(cur_bot):
 
-    #def dice(cur_bot):
 
-    # will this take as input multiple users?
-    #def alarm(cur_bot, user):
+    def weather_time(cur_bot, location='Rolla'):
+            # Set API Key
+        API_key = '0f5e7a1e7682ce843b443549718b5f95'
+        owm = OWM(API_key)
 
-    #def popular_message(cur_bot):
+            # Returns an "observation" of weather data (NOT A FORECAST)
+        obs = owm.weather_at_place(location)
+            # Get time the weather was received at
+        weather_time = obs.get_reception_time(timeformat='iso')
 
-    #def poll(cur_bot):
+        cur_bot.post("Time: " + weather_time)
 
-    # Nerdy/Dorky Commands
 
-    #def xkcd(cur_bot):
+
+    def weather_extreme(cur_bot, location='Rolla'):
+            # Set API Key
+        API_key = '0f5e7a1e7682ce843b443549718b5f95'
+        owm = OWM(API_key)
+
+        # Returns an "observation" of weather data (NOT A FORECAST)
+        obs = owm.weather_at_place(location)
+            # Gets actual weather data
+        w = obs.get_weather()
+        temp_f_dict = w.get_temperature('fahrenheit') #this is a dictionary of fahrenheit values
+
+            # find the high for the day in the temperature dictionary
+        temp_high = 0
+        for key in temp_f_dict:
+            if key == 'temp_max':
+                temp_high = temp_f_dict[key]
+            # find the low for the day in the temperature dictionary
+        temp_low = 0
+        for key in temp_f_dict:
+            if key == 'temp_min':
+                temp_low = temp_f_dict[key]
+
+        l = obs.get_location()
+        location_name = l.get_name()
+        print("User input location: " + location)
+        print("The object location: " + location_name)
+
+        cur_bot.post("High: " + str(temp_high) + "°F\nLow: " + str(temp_low) + "°F")
+
+
+
+    def weather_sun(cur_bot, location='Rolla'):
+                # Set API Key
+        API_key = '0f5e7a1e7682ce843b443549718b5f95'
+        owm = OWM(API_key)
+
+            # Returns an "observation" of weather data (NOT A FORECAST)
+        obs = owm.weather_at_place(location)
+            # Gets actual weather data
+        w = obs.get_weather()
+
+            #Get sunrise and sunset times
+        sunrise = w.get_sunrise_time('iso')
+        sunset = w.get_sunset_time('iso')
+
+            # Object for getting location
+        l = obs.get_location()
+        location_name = l.get_name()
+
+        cur_bot.post("In " + location_name + ":\nSunrise: " + sunrise + "\nSunset: " + sunset)
+
+
+
+
+
+
+    def weather_specific(cur_bot, location='Rolla'):
+                    # Set API Key
+        API_key = '0f5e7a1e7682ce843b443549718b5f95'
+        owm = OWM(API_key)
+
+            # Returns an "observation" of weather data (NOT A FORECAST)
+        obs = owm.weather_at_place(location)
+            # Gets actual weather data
+        w = obs.get_weather()
+
+            #Get values
+        conditions = w.get_detailed_status()
+        temp_f_dict = w.get_temperature('fahrenheit')
+        wind_dict = w.get_wind()
+        rain = w.get_rain()
+        humidity = w.get_humidity() # humidity percentage
+        pressure = w.get_pressure() # atmospheric pressure
+
+            #Get values out of dictionaries
+        temp_current = 0
+        for key in temp_f_dict:
+            if key == 'temp':
+                temp_current = temp_f_dict[key]
+
+        wind_degree = 0
+        wind_speed_ms = 0 # in meters/second
+        for key in wind_dict:
+            if key == 'deg':
+                wind_degree = wind_dict[key]
+            if key == 'speed':
+                wind_speed_ms = wind_dict[key]
+
+
+
+            #Decide wind dirction, all values rounded
+            '''
+                N: 0 - 23 and 383 - 0
+                NE: 23 - 68
+                E: 68 - 113
+                SE: 113 - 158
+                S: 158 - 203
+                SW: 203 - 248
+                W: 248 - 293
+                NW: 293 - 338
+            '''
+        wind_direction = "in your face"
+        if (wind_degree >= 0 and wind_degree <= 23) or (wind_degree > 338 and wind_degree <= 360):
+            wind_direction = "N"
+        elif (wind_degree > 23 and wind_degree <= 68):
+            wind_direction = "NE"
+        elif (wind_degree > 68 and wind_degree <= 113):
+            wind_direction = "E"
+        elif (wind_degree > 113 and wind_degree <= 158):
+            wind_direction = "SE"
+        elif (wind_degree > 158 and wind_degree <= 203):
+            wind_direction = "S"
+        elif (wind_degree > 203 and wind_degree <= 248):
+            wind_direction = "SW"
+        elif (wind_degree > 248 and wind_degree <= 293):
+            wind_direction = "W"
+        elif (wind_degree > 293 and wind_degree <= 338):
+            wind_direction = "NW"
+
+            # Change wind to mph
+        wind_speed_mph = wind_speed_ms * 2.23694
+            # Round to one decimal place
+        wind_speed_mph = round(wind_speed_mph, 1)
+
+        cur_bot.post(conditions.capitalize() + "\nTemperature: " + str(temp_current) + "°F\nWind: " + str(wind_speed_mph) + "mph " + wind_direction + "\nRain: " + str(rain) + "\nHumidity: " + str(humidity) + "\nPressure: " + str(pressure))
+
+
+
+
+
+
+
+
+
 
     # Holiday Commands
-
-    #def days_to_xmas(cur_bot):
 
         #S&T Commands
 
@@ -152,13 +323,14 @@ def groupme(request):
 
             if message == '##help':
                 bot.post('Here is the list of commands: ' +
+                        '\n##ping -- Pong!' +
                         '\n##sleep -- Turns off the bot for the night.' +
-                        '\n##cookie <person> -- Give a cookie to somebody.' +
+                        '\n##cookie [person] -- Give a cookie to somebody.' +
                         '\n##lennyface -- Post a random lenny face.' +
                         '\n##fliptable -- You flip a table.' +
-                        '\n##ping -- Pong!'
-                        '\n##weather -- Gets the weather' +
-                        '\n##daze -- Posts how many daze until St. Pats.'
+                        '\n##daze -- Posts how many daze until St. Pats.' +
+                        '\n##d[number] -- Rolls a dice with [number] sides.' +
+                        '\n##weather <location> -- Gets the weather for the given city name.'
                         #'\n##random  -- Does a random thing.' +
                         #'\n##d<number> -- Rolls a d<number> ex: ##d20' +
                         #'\n##roll <num1>d<num2> -- Rolls <num1> d<num2> ex: ##roll 4d20'
@@ -180,11 +352,19 @@ def groupme(request):
                 give_cookie(cur_bot=bot, post_author=name, user=cased_message[9:])
 
 
+            elif message == '##party':
+                party(cur_bot=bot, post_author=name)
+
+
+            elif message == '##sentient':
+                sentient(cur_bot=bot, post_author=name)
+
+
             elif message == '##lennyface':
                 lenny_face(cur_bot=bot)
 
 
-            elif message == '##fliptable' or message == '##tableflip':
+            elif message == '##fliptable' or message == '##tableflip' or message == '##flip table':
                 table_flip(cur_bot=bot)
 
 
@@ -192,17 +372,60 @@ def groupme(request):
                 ping(cur_bot=bot)
 
 
-            elif message[:9] == '##weather':
-                if message[10:15].isnumeric():
-                    weather(zip_code=message[10:15], cur_bot=bot, post_author=name)
-                elif message[10:].isalpha():
-                    weather(location=message[10:], cur_bot=bot, post_author=name)
-                else:
-                    #weather(cur_bot=bot, location='Rolla') # for my own weather function
-                    weather(zip_code='65401', cur_bot=bot, post_author=name)  # for Austin's weather function
 
             elif message == '##daze' or message == '##daze till pats' or message == '##days till pats' or message == '##daze till pat\'s' or message == '##daze til pats':
                 daze(cur_bot=bot)
+
+
+
+            elif message[:3] == '##d' and message[3:].isnumeric():
+                if message[3:] == '0':
+                    bot.post('Rolling a d0\n' +
+                             name + ' has imploded the universe.')
+                else:
+                    try:
+                        bot.post('Rolling a d' + message[3:] + '...\n' +
+                                 name + ' rolled a ' + str(random.randint(1, int(message[3:]))))
+                    except ValueError:
+                        bot.post('Invalid Number')
+
+
+            elif message[:9] == '##weather':
+                if message[10:] == '':
+                    weather(cur_bot=bot, location='Rolla')
+                elif message[10:15].isnumeric():
+                    weather(location=message[10:15], cur_bot=bot)
+                else:
+                    weather(location=message[10:], cur_bot=bot)
+
+            elif message[:6] == '##high':
+                if message[7:].isalpha():
+                    weather_extreme(location=message[7:], cur_bot=bot)
+                else:
+                    weather_extreme(cur_bot=bot, location='Rolla')
+
+            elif message[:5] == '##low':
+                if message[6:].isalpha():
+                    weather_extreme(location=message[6:], cur_bot=bot)
+                else:
+                    weather_extreme(cur_bot=bot, location='Rolla')
+
+            elif message[:13] == "##meteorology":
+                if message[14:].isalpha():
+                    weather_specific(location=message[14:], cur_bot=bot)
+                else:
+                    weather_specific(cur_bot=bot, location='Rolla')
+
+            elif message == '##sunrise' or message == '##sunset' or message == '##sun':
+                weather_sun(cur_bot=bot, location='Rolla')
+
+            elif message[:13] == '##owmtime':
+                weather_time(cur_bot=bot, location='Rolla')
+
+
+
+
+
 
         except AttributeError as e:
             print('Bot has encountered an error')
